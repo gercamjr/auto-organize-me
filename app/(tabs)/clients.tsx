a12b0ec2f2eb451c726e5f4499c1beb3009e98ad
Gerardo Camorlinga Jr <gerardo.camorlinga@guardmetrics.com>
2025-02-10 16:17:45 -0600

    adding crud operations - scaffolding

--- app/(tabs)/clients.tsx
+++ app/(tabs)/clients.tsx
@@ -1,9 +1,38 @@
-import { Text, View, StyleSheet } from 'react-native'
+import React, { useEffect, useState } from 'react'
+import { Text, View, StyleSheet, FlatList, Button } from 'react-native'
+import { getClients, addClient } from '../dbOperations'
+import { Clients } from '../types'
+import { Link } from 'expo-router'
 
 export default function ClientsScreen() {
+  const [clients, setClients] = useState<Clients[]>([])
+
+  useEffect(() => {
+    fetchClients()
+  })
+
+  async function fetchClients() {
+    const clients: Clients[] = await getClients()
+    setClients(clients)
+  }
+
   return (
     <View style={styles.container}>
       <Text style={styles.text}>Clients</Text>
+      <Link href='/add-client' style={styles.button}>
+        Add Client
+      </Link>
+      <FlatList
+        data={clients}
+        keyExtractor={(item) => item.id.toString()}
+        renderItem={({ item }) => (
+          <Link href={{ pathname: '/client/:id', params: { id: item.id } }}>
+            <View style={styles.clientItem}>
+              <Text style={styles.text}>{item.name}</Text> <Text style={styles.text}>{item.address}</Text>
+            </View>
+          </Link>
+        )}
+      />
     </View>
   )
 }
@@ -16,6 +45,17 @@ const styles = StyleSheet.create({
     alignItems: 'center',
   },
   text: {
+    color: '#fff',
+  },
+  clientItem: {
+    marginVertical: 8,
+    padding: 16,
+    backgroundColor: '#f9f9f9',
+    borderRadius: 8,
+  },
+  button: {
+    fontSize: 20,
+    textDecorationLine: 'underline',
     color: '#fff',
   },
 })
--- app/dbOperations.ts
+++ app/dbOperations.ts
@@ -1,13 +1,5 @@
 import db from './database'
-
-export interface Clients {
-  id: number
-  name: string
-  address: string
-  phonenumber: string
-  created: string
-  updated: string
-}
+import { Clients, Vehicles, Jobs } from './types'
 
 // get clients
 export const getClients = async (): Promise<Clients[]> => {
@@ -56,4 +48,151 @@ export const updateClient = async (id: number, name: string, address: string, ph
     phonenumber,
     id,
   ])
+}
+
+// get vehicles
+export const getVehicles = async (): Promise<Vehicles[]> => {
+  const results: Vehicles[] = await db.getAllAsync('SELECT * FROM vehicles ORDER BY created DESC')
+  return results
+}
+
+// get vehicle by id and include jobs and client
+export const getVehicleByIdAndJobsAndClient = async (id: number): Promise<Vehicles[] | null> => {
+  const results: Vehicles[] = await db.getAllAsync(
+    'SELECT * FROM vehicles, jobs, clients WHERE vehicles.id = $vehicleid INNER JOIN AS jobs ON vehicles.id = jobs.vehicle_id INNER JOIN AS clients ON vehicles.client_id = clients.id',
+    { vehicleid: id }
+  )
+  if (results.length > 0) {
+    return results
+  }
+  return null
+}
+
+// add vehicle
+export const addVehicle = async (
+  client_id: number,
+  make: string,
+  model: string,
+  year: number,
+  photos: string[]
+): Promise<number | null> => {
+  const results = await db.runAsync(
+    'INSERT INTO vehicles (client_id, make, model, year, photos) VALUES ($client_id, $make, $model, $year, $photos)',
+    {
+      client_id: client_id,
+      make: make,
+      model: model,
+      year: year,
+      photos: JSON.stringify(photos),
+    }
+  )
+  if (results.lastInsertRowId) {
+    return results.lastInsertRowId
+  }
+  return null
+}
+
+// delete vehicle
+export const deleteVehicle = async (id: number): Promise<void> => {
+  await db.runAsync('DELETE FROM vehicles WHERE id = ?', [id])
+}
+
+// update vehicle
+export const updateVehicle = async (
+  id: number,
+  client_id: number,
+  make: string,
+  model: string,
+  year: number,
+  photos: string[]
+): Promise<void> => {
+  await db.runAsync('UPDATE vehicles SET client_id = ?, make = ?, model = ?, year = ?, photos = ? WHERE id = ?', [
+    client_id,
+    make,
+    model,
+    year,
+    JSON.stringify(photos),
+    id,
+  ])
+}
+
+// get jobs
+export const getJobs = async (): Promise<Jobs[]> => {
+  const results: Jobs[] = await db.getAllAsync('SELECT * FROM jobs ORDER BY created DESC')
+  return results
+}
+
+// get job by id and include vehicle and client
+export const getJobByIdAndVehicleAndClient = async (id: number): Promise<Jobs[] | null> => {
+  const results: Jobs[] = await db.getAllAsync(
+    'SELECT * FROM jobs, vehicles, clients WHERE jobs.id = $jobid INNER JOIN AS vehicles ON jobs.vehicle_id = vehicles.id INNER JOIN AS clients ON vehicles.client_id = clients.id',
+    { jobid: id }
+  )
+  if (results.length > 0) {
+    return results
+  }
+  return null
+}
+
+// add job
+export const addJob = async (
+  vehicle_id: number,
+  description: string,
+  cost: number,
+  photos: string[]
+): Promise<number | null> => {
+  const results = await db.runAsync(
+    'INSERT INTO jobs (vehicle_id, description, cost) VALUES ($vehicle_id, $description, $cost, $photos)',
+    {
+      vehicle_id: vehicle_id,
+      description: description,
+      cost: cost,
+      photos: JSON.stringify(photos),
+    }
+  )
+  if (results.lastInsertRowId) {
+    return results.lastInsertRowId
+  }
+  return null
+}
+
+// delete job
+export const deleteJob = async (id: number): Promise<void> => {
+  await db.runAsync('DELETE FROM jobs WHERE id = ?', [id])
+}
+
+// update job
+export const updateJob = async (
+  id: number,
+  vehicle_id: number,
+  description: string,
+  cost: number,
+  photos: string[]
+): Promise<void> => {
+  await db.runAsync('UPDATE jobs SET vehicle_id = ?, description = ?, cost = ?, photos = ? WHERE id = ?', [
+    vehicle_id,
+    description,
+    cost,
+    JSON.stringify(photos),
+    id,
+  ])
+}
+
+// export db operations
+export default {
+  getClients,
+  getClientByIdAndVehicles,
+  addClient,
+  deleteClient,
+  updateClient,
+  getVehicles,
+  getVehicleByIdAndJobsAndClient,
+  addVehicle,
+  deleteVehicle,
+  updateVehicle,
+  getJobs,
+  getJobByIdAndVehicleAndClient,
+  addJob,
+  deleteJob,
+  updateJob,
 }
